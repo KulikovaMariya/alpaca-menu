f8f6bd473ba670ec4bb478faef25a23f8d285059
Maria Kulikova <m.kulikova@aeronavigator.ru>
2021-12-30 20:51:34 +0300

    feat: add style for tablet resolution (unimplemented)

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,9 @@
 import {testFunction} from './scripts/test';
 testFunction();
+
+const btn = document.querySelector('.header__burger');
+
+btn.onclick = () => {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('nav--active')
+};
